79de524414612a052fd1ca9c80551336db805bfc
trotsm <trots86@gmail.com>
2019-03-20 01:08:19 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: FirstComponent },
+  { path: 'secondpage', component: SecondComponent },
+  { path: 'thirdpage', component: ThirdComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
